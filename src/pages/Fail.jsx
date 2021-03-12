570e31184d49f367ac274c1ce2967f493c0fbe50
ho dev <hodev.dev@gmail.com>
2021-03-12 10:50:52 +0330

    change

---
+++ src/pages/Fail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Fail = () => {
+  return (
+    <div className={"w-full h-auto text-6xl text-black"}>
+      fail
+    </div>
+  )
+}
+
+export default Fail
---
+++ src/pages/Success.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Success = () => {
+  return (
+    <div className={"w-full h-auto text-6xl text-black"}>
+      success
+    </div>
+  )
+}
+
+export default Success
